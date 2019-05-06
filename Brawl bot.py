5d754b0b107c03aa9bce864f7a721af9ef9878f5
Surekusan <44528508+Surekusan@users.noreply.github.com>
2019-05-06 18:52:09 +0200

    Add files via upload

---
+++ Brawl bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import os
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = ".")
+@client.event
+async def on_ready():
+    print("Danke das du den Brawl Zone Bot verwendest")
+    await client.change_presence(game=discord.Game(name=".help"))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('.Hi'):
+        msg = 'Hi {0.author.mention}' .format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('.test'):
+        msg = 'tete {0.author.mention}' .format(message)
+        await client.send_message(message.channel, msg)
+
+client.run(os.getenv('NTc0OTUzOTIwNTk0NDQ0Mjk4.XNA5oQ.6zoPVEowKcwLFQIuYLhnAQah9Gw'))
+
+                    
